dc867fc532398a3ae4e270fb24aab40d9ef483de
Elwodbe <165378786+Elwodbe@users.noreply.github.com>
2024-05-08 10:15:12 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+
+
+let userName1 = prompt(" Ismingizni kiriting")
+let userSurname = prompt("Familyangizni kiriting")
+let userage = prompt("Yoshingizni kiriting")
+
+console.log(userName1, userSurname, userage);
